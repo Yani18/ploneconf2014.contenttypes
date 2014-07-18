a24b3f649a309d45d090cdd8b91951a173159c9e
Yani <yaninamuradas@gmail.com>
2014-07-18 10:19:26 -0430

    first commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='ploneconf2014.contenttypes',
+      version=version,
+      description="Example Diazo Theme Package",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='contenttypes, package, practica',
+      author='Yanina M',
+      author_email='yaninamuradas@gmail.com',
+      url='https://github.com/Yani18/ploneconf2014.contenttypes.git',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ploneconf2014'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
